6d2bc2899b4233526be387de9c3dcda588e66821
Ben Drucker <bvdrucker@gmail.com>
2015-01-24 21:38:34 -0500

    instantiate nodeback stub before each test

--- test/index.js
+++ test/index.js
@@ -15,34 +15,39 @@ describe('angular-q-promisify', function () {
 
   describe('#promisify', function () {
 
+    var nodeback;
+    beforeEach(function () {
+      nodeback = sinon.stub();
+    })
+
     it('promisifes a nodeback', function () {
-      var nodeback = sinon.stub().yields(null);
+      nodeback.yields(null);
       expect($q.promisify(nodeback)()).to.be.fulfilled;
       $timeout.flush();
     });
 
     it('fulfills with a value', function () {
-      var nodeback = sinon.stub().yields(null, 'foo');
+      nodeback.yields(null, 'foo');
       expect($q.promisify(nodeback)()).to.eventually.equal('foo');
       $timeout.flush();
     });
 
     it('returns multiple values as an array', function () {
-      var nodeback = sinon.stub().yields(null, 'foo', 'bar');
+      nodeback.yields(null, 'foo', 'bar');
       expect($q.promisify(nodeback)()).to.eventually.deep.equal(['foo', 'bar']);
       $timeout.flush();
     });
 
     it('rejects with errors', function () {
       var err = new Error();
-      var nodeback = sinon.stub().yields(err);
+      nodeback.yields(err);
       expect($q.promisify(nodeback)()).to.be.rejectedWith(err);
       $timeout.flush();
     });
 
     it('rejects with errors thrown by the callback', function () {
       var err = new Error();
-      var nodeback = sinon.stub().throws(err);
+      nodeback.throws(err);
       expect($q.promisify(nodeback)()).to.be.rejectedWith(err);
       $timeout.flush();
     });
